8f494d285b6c2c2c0e7e275b6f6ce112d8c1275a
potvik <yuriy@harmony.one>
2024-08-31 23:28:23 +0300

    autocompounding contracts

--- src/config/tokens.ts
+++ src/config/tokens.ts
@@ -32,7 +32,7 @@ export const TOKENS: { [chainId: number]: Token[] } = {
     {
       name: "GMX XP",
       symbol: "GXP",
-      address: "0x7C54F8d25F224DeE7A0FA7583f621E9AebE24DfE",
+      address: "0xf0C932F189A503AE3B46F3b80Df93244ef7eC2B9",
       decimals: 18,
       imageUrl: "https://github.com/gmx-io/gmx-assets/blob/main/GMX-Assets/PNG/GLP_LOGO%20ONLY.png?raw=true",
       reservesUrl: "https://portfolio.nansen.ai/dashboard/gmx?chain=HARMONY",
